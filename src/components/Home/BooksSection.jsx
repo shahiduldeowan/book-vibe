4670a3d10e89801f68e909a3b4c2982abb3e0b19
Shahidul Deowan <deowanshahidul@gmail.com>
2024-03-28 04:46:12 +0600

    add books grid view and loader

--- src/components/Home/BooksSection.jsx
+++ src/components/Home/BooksSection.jsx
@@ -1,16 +1,24 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigation } from "react-router-dom";
+import BookGridItemCard from "../Book/BookGridItemCard";
 import Loader from "../Loader/Loader";
 import SectionTitle from "../SectionTitle/SectionTitle";
 
 const BooksSection = () => {
   const books = useLoaderData();
-  console.log("books :>> ", books);
-  if (books) {
+  const navigation = useNavigation();
+
+  if (navigation.state === "loading") {
     return <Loader />;
   }
+
   return (
     <>
       <SectionTitle title="Books" />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {books.map((book) => {
+          return <BookGridItemCard book={book} key={book.bookId} />;
+        })}
+      </div>
     </>
   );
 };
