a1c1d572b903f37e1ada497fe30104e306e05645
Shahidul Deowan <deowanshahidul@gmail.com>
2024-03-28 04:45:01 +0600

    Enhance

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 const SectionTitle = ({ title, alignment = "text-center" }) => {
   return (
     <h2
-      className={`${alignment} text-2xl lg:text-3xl xl:text-4xl font-bold mb-6 lg:mb-10`}
+      className={`font-playfair-display ${alignment} text-2xl lg:text-3xl xl:text-4xl font-bold mb-6 lg:mb-10`}
     >
       {title}
     </h2>
