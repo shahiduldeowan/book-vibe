00f65336d4f4615390d8c69f6394d31385346fe4
Shahidul Deowan <deowanshahidul@gmail.com>
2024-03-27 12:24:05 +0600

    add navbar component

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
 
 const MainLayout = () => {
   return (
     <>
-      <div>Nav bar</div>
+      <Navbar />
       <Outlet />
     </>
   );
