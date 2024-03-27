5765a7f6fa611450300db89ac7b151afe2b61364
Shahidul Deowan <deowanshahidul@gmail.com>
2024-03-27 11:23:04 +0600

    enhance

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>Book Review</h1>
+      <h2>For test</h2>
+      <p>{count}</p>
+      <button
+      onClick={()=> setCount(count+1)}
+       type="button" 
+       className="btn"
+       >Counter</button>
     </>
   )
 }
