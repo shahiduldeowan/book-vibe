a4d8b86a9dcb36e1d859a4fc64d2aca841eab9ee
Shahidul Deowan <deowanshahidul@gmail.com>
2024-03-27 22:27:09 +0600

    add loader component

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import { HashLoader } from "react-spinners";
+
+const Loader = () => {
+  return (
+    <div className=" w-full flex justify-center items-start">
+      <HashLoader color="#23BE0A" size={60} />
+    </div>
+  );
+};
+
+export default Loader;
