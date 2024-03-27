e57396983b7b3a9a6624dc8de2a9f2b480f53cc6
Shahidul Deowan <deowanshahidul@gmail.com>
2024-03-27 22:28:52 +0600

    add BooksSection in home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
-import BannerSection from "../components/Banner/BannerSection";
+import BannerSection from "../components/Home/BannerSection";
+import BooksSection from "../components/Home/BooksSection";
 
 const HomePage = () => {
   return (
     <div>
       <BannerSection />
+      <BooksSection />
       <h1>Home page</h1>
     </div>
   );
