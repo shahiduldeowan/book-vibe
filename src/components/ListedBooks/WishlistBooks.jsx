59d86a7d8674dfd0368272816c3e034d4da07f2d
Shahidul Deowan <deowanshahidul@gmail.com>
2024-03-28 20:00:54 +0600

    sorted by descending order

--- src/components/ListedBooks/WishlistBooks.jsx
+++ src/components/ListedBooks/WishlistBooks.jsx
@@ -1,10 +1,15 @@
 import { useContext, useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
-import { SortedBooksContext } from "../../pages/ListedBooksPage";
+import {
+  SelectedCategoryContext,
+  SortedBooksContext,
+} from "../../pages/ListedBooksPage";
+import ListedBook from "./ListedBook";
 
 const WishlistBooks = () => {
   const [books, setBooks] = useState([]);
   const sortedBooks = useContext(SortedBooksContext);
+  const selectedCategory = useContext(SelectedCategoryContext);
   const loadedIds = useLoaderData();
 
   useEffect(() => {
@@ -12,11 +17,29 @@ const WishlistBooks = () => {
       const myBooks = loadedIds.map((id) =>
         sortedBooks.find((book) => book.bookId === id)
       );
+      if (selectedCategory) {
+        if (selectedCategory === "Rating") {
+          myBooks.sort((a, b) => b.rating - a.rating);
+        }
+        if (selectedCategory === "Number of pages") {
+          myBooks.sort((a, b) => b.totalPages - a.totalPages);
+        }
+        if (selectedCategory === "Publisher year") {
+          myBooks.sort((a, b) => b.yearOfPublishing - a.yearOfPublishing);
+        }
+      }
       setBooks(myBooks);
     }
-  }, [loadedIds, sortedBooks]);
+  }, [loadedIds, sortedBooks, selectedCategory]);
 
-  return <div className="flex flex-col gap-6">{books.length}</div>;
+  return (
+    <div className="flex flex-col gap-6">
+      {books &&
+        books.map((book, idx) => {
+          return <ListedBook key={idx} book={book} />;
+        })}
+    </div>
+  );
 };
 
 export default WishlistBooks;
