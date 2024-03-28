63339bc91a4ca8ca62b26e2914345c97f53954ce
Shahidul Deowan <deowanshahidul@gmail.com>
2024-03-28 17:54:00 +0600

    add error page in route

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,4 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
+import ReadBooks from "../components/ListedBooks/ReadBooks";
+import WishlistBooks from "../components/ListedBooks/WishlistBooks";
 import MainLayout from "../layouts/MainLayout";
 import BestSellerPage from "../pages/BestSellerPage";
 import BookDetailsPage from "../pages/BookDetailsPage";
@@ -7,6 +9,7 @@ import HomePage from "../pages/HomePage";
 import ListedBooksPage from "../pages/ListedBooksPage";
 import PagesToReadPage from "../pages/PagesToReadPage";
 import SearchPage from "../pages/SearchPage";
+import { getReadBooks, getWishlistBooks } from "../utils";
 
 export const router = createBrowserRouter([
   {
@@ -27,6 +30,19 @@ export const router = createBrowserRouter([
       {
         path: "/listed-books",
         element: <ListedBooksPage />,
+        loader: () => fetch(`books.json`),
+        children: [
+          {
+            index: true,
+            element: <ReadBooks />,
+            loader: () => new Promise((resolve) => resolve(getReadBooks())),
+          },
+          {
+            path: "wishlist",
+            element: <WishlistBooks />,
+            loader: () => new Promise((resolve) => resolve(getWishlistBooks())),
+          },
+        ],
       },
       {
         path: "/pages-to-read",
