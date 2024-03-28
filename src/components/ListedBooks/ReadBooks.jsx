e10b1971470e2438a5ae4df79b4a5dc60a37b58a
Shahidul Deowan <deowanshahidul@gmail.com>
2024-03-28 20:00:34 +0600

    books sorted by descending order

--- src/components/ListedBooks/ReadBooks.jsx
+++ src/components/ListedBooks/ReadBooks.jsx
@@ -1,11 +1,15 @@
 import { useContext, useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
-import { SortedBooksContext } from "../../pages/ListedBooksPage";
+import {
+  SelectedCategoryContext,
+  SortedBooksContext,
+} from "../../pages/ListedBooksPage";
 import ListedBook from "./ListedBook";
 
 const ReadBooks = () => {
   const [books, setBooks] = useState([]);
   const sortedBooks = useContext(SortedBooksContext);
+  const selectedCategory = useContext(SelectedCategoryContext);
   const loadedIds = useLoaderData();
 
   useEffect(() => {
@@ -13,9 +17,21 @@ const ReadBooks = () => {
       const myBooks = loadedIds.map((id) =>
         sortedBooks.find((book) => book.bookId === id)
       );
+      if (selectedCategory) {
+        if (selectedCategory === "Rating") {
+          myBooks.sort((a, b) => b.rating - a.rating);
+        }
+        if (selectedCategory === "Number of pages") {
+          myBooks.sort((a, b) => b.totalPages - a.totalPages);
+        }
+        if (selectedCategory === "Publisher year") {
+          myBooks.sort((a, b) => b.yearOfPublishing - a.yearOfPublishing);
+        }
+      }
       setBooks(myBooks);
     }
-  }, [loadedIds, sortedBooks]);
+  }, [loadedIds, sortedBooks, selectedCategory]);
+
   return (
     <div className="flex flex-col gap-6">
       {books &&
